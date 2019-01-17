34c0526b9ef646bc156f61948a5df257b67ffc74
Rebeca Bordini <rebeca.bordini@corp.globo.com>
2019-01-17 13:22:15 -0200

    Updates identity_final.csv file

--- v0.py
+++ v0.py
@@ -1,16 +1,17 @@
 # -*- coding:utf-8 -*-
 import csv
+from dateutil.parser import parse
 
-# 1. Ler arquivo já consolidado com os eleitos da eleição 2018
 # 2. Criar um POST (na ontologia Agents) para cada vaga de deputado/senador, por estado. \
 #    com start date 01/02/2019 e end date 31/1/2024), e um estado associado.
-# 3. Consultar o tabelão (pelo nome civil) e ver se ele já aparece. Se aparecer,  \
-#    n precisa fazer nada. Se não aparecer, tem que criar uma nova entrada, gerando uma URI para ele.
 # 4. Criar um JSON com uma entrada para politico, incluindo a URL dele, o nome do candidato, \
 #    o nome civil, a data de nascimento, a url do partido (ORG),   e de um  POST que vc criou para o estado dele (\
 #    usar um diferente para cada eleito).
 # 5. A partir deste JSON, gerar um mapeamento no KARMA (https://usc-isi-i2.github.io/karma/) \
 #    para gerar o RDF conforme a ontologia AGENTS que está no diretório.
+
+## 81 senadores
+## 513 deputados federais
 
 
 START_DATE = '01/02/2019'
@@ -36,10 +37,11 @@ class Identity:
     def add_senator(self, senator):
         self.senators.append(senator)
 
-    def find_deputy(self, name):
-        for deputy in self.deputies:
-            if deputy.name == name:
-                return deputy
+    def find(self, name):
+        congressman_list = self.deputies + self.senators
+        for congressman in congressman_list:
+            if congressman.name == name:
+                return congressman
         return None
 
     def get_all_deputies(self):
@@ -48,29 +50,49 @@ class Identity:
     def get_all_senators(self):
         return self.senators
 
+    def update_data(self, uri, data):
+        with open('./identity_final.csv', 'a') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=';')
+            formated_birth_date = parse(data.birth_date)
+            spamwriter.writerow([uri, '', formated_birth_date.strftime('%Y-%m-%d'), '', data.name, '', '', '', ''])
+
 
 class ElectionResults:
+
+    POSTS = ['SENADOR', 'DEPUTADO FEDERAL']
+    ELECTED_FLAGS = ['ELEITO', 'ELEITO POR MEDIA', 'ELEITO POR QP']
+
     def __init__(self):
         self.elected = []
 
-    def get_all_elected(self):
         with open('./candidatos+resultados+2018.csv', 'rb') as csvfile:
             spamreader = csv.DictReader(csvfile, delimiter=',')
             for row in spamreader:
                 self.add_elected(Elected(row))
-        return self.elected
 
     def add_elected(self, elected):
-        self.elected.append(elected)
+        if elected.post in self.POSTS and elected.shift in self.ELECTED_FLAGS:
+            self.elected.append(elected)
+
+    def get_all_elected(self):
+        return self.elected
 
 
 class Elected:
 
     def __init__(self, data):
         self.name = data.get('nome', '')
+        self.birth_date = data.get('data_nascimento', '')
+        self.cpf = data.get('cpf', '')
+        self.electoral_unity = data.get('sigla_uf', '')
+        self.party = data.get('numero_partido', '')
+        self.party_name = data.get('sigla_partido', '')
+        self.post = data.get('descricao_cargo', '')
+        self.shift = data.get('descricao_totalizacao_turno', '')
+        self.election_year = data.get('ano_eleicao', '')
 
     def __repr__(self):
-        return "{civil_name}".format(civil_name=self.name)
+        return str(vars(self))
 
 
 class Deputy:
@@ -98,19 +120,16 @@ class Senator:
         return "{civil_name}".format(civil_name=self.civil_name)
 
 
-def create_post_instance(congressman, state):
-    pass
-
-
-def get_uri():
-    pass
-
-
-def create_uri():
-    pass
+def generate_uri():
+    import uuid
+    return str(uuid.uuid4())
 
 
 if __name__ == '__main__':
     identity = Identity()
-    deputies = identity.get_all_deputies()
-
+    election_results = ElectionResults()
+    elected_2018 = election_results.get_all_elected()
+    for elected in elected_2018:
+        if not identity.find(elected.name):
+            uri = generate_uri()
+            identity.update_data(uri, elected)
