d58ec9e2117bfb61a2013bb1669272bf5e439250
Rebeca Bordini <rebeca.bordini@corp.globo.com>
2019-01-17 00:04:10 -0200

    Adds senators found in identity table

--- v0.py
+++ v0.py
@@ -22,22 +22,31 @@ class Identity:
         self.deputies = []
         self.senators = []
 
-    def get_all_deputies(self):
         with open('./identity.csv', 'rb') as csvfile:
             spamreader = csv.DictReader(csvfile, delimiter=';')
             for row in spamreader:
                 if row.get('cam:ideCadastro'):
                     self.add_deputy(Deputy(row))
-        return self.deputies
+                if row.get('sen:CodigoParlamentar'):
+                    self.add_senator(Senator(row))
 
     def add_deputy(self, deputy):
         self.deputies.append(deputy)
+
+    def add_senator(self, senator):
+        self.senators.append(senator)
 
     def find_deputy(self, name):
         for deputy in self.deputies:
             if deputy.name == name:
                 return deputy
         return None
+
+    def get_all_deputies(self):
+        return self.deputies
+
+    def get_all_senators(self):
+        return self.senators
 
 
 class ElectionResults:
@@ -48,8 +57,7 @@ class ElectionResults:
         with open('./candidatos+resultados+2018.csv', 'rb') as csvfile:
             spamreader = csv.DictReader(csvfile, delimiter=',')
             for row in spamreader:
-                if row.get('nome'):
-                    self.add_elected(Elected(row))
+                self.add_elected(Elected(row))
         return self.elected
 
     def add_elected(self, elected):
@@ -74,6 +82,17 @@ class Deputy:
         self.id = data.get('cam:ideCadastro', '')
         self.civil_name = data.get('cam:nomeCivil', '')
         self.name = data.get('cam:nomeParlamentarAtual', '')
+
+    def __repr__(self):
+        return "{civil_name}".format(civil_name=self.civil_name)
+
+
+class Senator:
+
+    def __init__(self, data):
+        self.resource_uri = data.get('slp:resource_uri', '')
+        self.civil_name = data.get('sen:NomeCompletoParlamentar', '')
+        self.name = data.get('sen:NomeParlamentar', '')
 
     def __repr__(self):
         return "{civil_name}".format(civil_name=self.civil_name)
