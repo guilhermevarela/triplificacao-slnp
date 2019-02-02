0b162b3022c9a65935c065e887de1b82d26c4575
Rebeca Bordini <rebeca.bordini@corp.globo.com>
2019-02-02 17:30:51 -0200

    Saves on .owl file

--- legislature_importer/classes/agent.py
+++ legislature_importer/classes/agent.py
@@ -1,13 +1,13 @@
 # -*- coding:utf-8 -*-
 from rdflib import BNode, ConjunctiveGraph, URIRef, Literal, Namespace, RDF, Graph
 
-from helpers import generate_uri
+from .helpers import generate_uri
 
-ONTOLOGY_PATH = './ontology/agent.rdf'
+ONTOLOGY_PATH = './ontology/agent-180422.owl'
 NAMESPACE = Namespace('http://www.w3.org/ns/org#')
 
 
-class Post:
+class Agent:
     def __init__(self):
         self.graph = Graph()
         self.graph.load(ONTOLOGY_PATH, format='n3')
@@ -15,8 +15,11 @@ class Post:
     def save(self):
         self.graph.serialize(ONTOLOGY_PATH, format='n3')
 
+    def new_jurisdiction(self, jurisdiction_name):
+        self.graph.add(jurisdiction_name, RDF.type, NAMESPACE['geonames'])
+
     def new_post(self, jurisdiction_name):
         post_uri = URIRef(generate_uri())
-        self.graph.add((post_uri, RDF.type, NAMESPACE['Post']))
+        self.graph.add(post_uri, RDF.type, NAMESPACE['Post'])
         self.graph.add((post_uri, NAMESPACE['hasJurisdiction'], Literal(jurisdiction_name)))
         self.save()
