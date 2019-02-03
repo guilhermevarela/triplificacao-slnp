8640121576943b9d24f70286e43da0c3288e68f3
Rebeca Bordini <rebeca.bordini@corp.globo.com>
2019-02-03 16:25:15 -0200

    Removes commented lines

--- legislature_importer/cli.py
+++ legislature_importer/cli.py
@@ -6,7 +6,6 @@ from .helpers import generate_uri
 
 
 ## TODO:
-#  Adicionar url do partido no JSON
 #  Mapeamento no Karma
 #  Refactoring
 #  Testes
@@ -27,11 +26,11 @@ def import_all_elected():
     party_list = PartyList()
 
     elected_2018 = election_results.get_all_elected()
-    # ontology.add_all_jurisdictions()
+    ontology.add_all_jurisdictions()
 
     for elected in elected_2018:
         post_uri = generate_uri()
-        # ontology.new_post(elected, post_uri)
+        ontology.new_post(elected, post_uri)
 
         previous_elected_congressman = identity.find(elected.name)
         if previous_elected_congressman:
