a2bb450324319db390df3c8b25d0456d7f20577f
Rebeca Bordini <rebeca.bordini@corp.globo.com>
2019-02-12 22:50:28 -0200

    Posts have start_date and end_date

--- CongressmanImporter/congressman_importer/classes/agent.py
+++ CongressmanImporter/congressman_importer/classes/agent.py
@@ -12,7 +12,7 @@ This module provides a object according to an ontology specified in T_BOX_PATH. 
 
 """
 
-from rdflib import URIRef, Namespace, RDF, Graph
+from rdflib import URIRef, Namespace, RDF, Graph, Literal, XSD
 
 from ..helpers import generate_timestamp
 
@@ -22,12 +22,17 @@ T_BOX_PATH = './ontology/agent-180422.owl'
 A_BOX_PATH = './generated-data/instances-{timestamp}.owl'.format(timestamp=generate_timestamp(string_format='%y%m%d'))
 
 NAMESPACE = Namespace('http://www.w3.org/ns/org#')
+SCHEMA_ORG_NAMESPACE = Namespace('http://schema.org/')
+
 POST_IRI_PREFIX ='http://www.w3.org/ns/org#Post'
 FEATURE_IRI_PREFIX = 'http://www.geonames.org/ontology#Feature'
 SNLP_IRI_PREFIX = 'http://www.seliganapolitica.org/resource'
 
 JURISDICTION_LIST = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT',
                      'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO']
+
+START_DATE="2019-02-01"
+END_DATE="2024-01-31"
 
 
 class Agent:
@@ -71,3 +76,8 @@ class Agent:
 
         self.graph.add((post_uri, RDF.type, NAMESPACE['Post']))
         self.graph.add((post_uri, NAMESPACE['hasJurisdiction'], jurisdiction_uri))
+
+        literal_start_date = Literal(START_DATE, datatype=XSD.date)
+        literal_end_date = Literal(END_DATE, datatype=XSD.date)
+        self.graph.add((post_uri, SCHEMA_ORG_NAMESPACE['startDate'], literal_start_date))
+        self.graph.add((post_uri, SCHEMA_ORG_NAMESPACE['endDate'], literal_end_date))
